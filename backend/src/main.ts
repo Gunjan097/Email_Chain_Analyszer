9cda13af6d3b8c98b84a2b652698948fedcc1740
Gunjan097 <gunjanmaheshwari09@gmail.com>
2025-09-07 18:57:50 +0530

    adding CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,12 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api'); // API prefix
+  app.setGlobalPrefix('api'); 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
   console.log(`Backend listening on http://localhost:${process.env.PORT || 3000}/api`);
 }
